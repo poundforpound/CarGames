2e2bfc9b687e9a9a3211c351c820b7f4ec53f9ef
poundforpound <a.zenit2014@yandex.ru>
2024-10-30 17:44:56 +0300

    add speed and fix Trees bag

--- script.js
+++ script.js
@@ -2,28 +2,48 @@
   let isPause = false;
   const car = document.querySelector('.car');
   const trees = document.querySelectorAll('.tree');
-  const speed = 3;
   let animationId = null;
   const firstTree = trees[0];
   animationId = requestAnimationFrame(startGame);
-
-  function getCoordinateY(element) {
-    const matrix = window.getComputedStyle(element).transform;
-    const matrixArray = matrix.split(',');
-    const coordinateY = parseFloat(matrixArray[matrixArray.length - 1]);
-    return coordinateY;
-  }
+  const allTrees = [];
+  trees.forEach((el) => {
+    const id = el.id;
+    const coordinates = getCoordinate(el);
+    allTrees.push({ element: el, coordinates });
+  });
   function startGame() {
     animationId = requestAnimationFrame(startGame);
     animationTrees();
-    // console.log('currentValue:', animationId);
   }
+
+  const biggestTree = allTrees.reduce((acc, el) => {
+    if (el.element.height > acc) {
+      return el.element.height;
+    }
+    return acc;
+  }, 0);
 
   function animationTrees() {
-    const newCoordinateY = getCoordinateY(firstTree) + speed;
-    firstTree.style.transform = `translateY(${newCoordinateY}px)`;
+    const speed = document.querySelector('.speed input').value;
+    allTrees.forEach((el) => {
+      const { element, coordinates } = el;
+      let newCoordinateY = coordinates.y + parseFloat(speed);
+
+      if (newCoordinateY > window.innerHeight) {
+        newCoordinateY = -biggestTree;
+      }
+      coordinates.y = newCoordinateY;
+      element.style.transform = `translate(${coordinates.x}px, ${newCoordinateY}px)`;
+    });
   }
 
+  function getCoordinate(element) {
+    const matrix = window.getComputedStyle(element).transform;
+    const matrixArray = matrix.split(',');
+    const coordinateY = parseFloat(matrixArray[matrixArray.length - 1]);
+    const coordinateX = parseFloat(matrixArray[matrixArray.length - 2]);
+    return { x: coordinateX, y: coordinateY };
+  }
   const button = document.querySelector('.btn');
   button.addEventListener('click', () => {
     isPause = !isPause;
